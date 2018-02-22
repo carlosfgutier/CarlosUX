143f7ebdc9bf9246dbc342820ce3389e818e6571
Carlos <carlosfgutier@gmail.com>
2018-02-21 21:49:26 -0600

    change to dos css onclick

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,20 +1,33 @@
-const toDos = $('#toDosContainer');
+const toDosContainer = $('#toDosContainer');
 const toDosTitle = $('#toDosTitle');
+const toDosSection = $('#toDosSection');
 let showToDos = false;
 
 if (showToDos === false) {
-	toDos.hide();
+	toDosContainer.hide();
+	toDosContainer.css('background-color', 'white');
 	toDosTitle.css('border-bottom', 'solid black 3px');
+
+	toDosSection.css('max-width', '100px');
+	toDosSection.css('max-height', '30px');
 }
 
 toDosTitle.click(function() {
 	if (showToDos === false) {
-		toDos.show();
-		toDosTitle.css('border-bottom', 'solid black 0px');
 		showToDos = true;
+		toDosContainer.show();
+		
+		toDosTitle.css('border-bottom', 'solid black 0px');
+		toDosSection.css('max-width', '20%');
+		toDosSection.css('max-height', '20%');
+		toDosSection.css('background-color', 'white');
+		
 	} else {
-		toDos.hide();
-		toDosTitle.css('border-bottom', 'solid black 3px');
 		showToDos = false;
+
+		toDosContainer.hide();
+		toDosTitle.css('border-bottom', 'solid black 3px');
+		toDosSection.css('max-width', '100px');
+		toDosSection.css('max-height', '30px');
 	}
 });
